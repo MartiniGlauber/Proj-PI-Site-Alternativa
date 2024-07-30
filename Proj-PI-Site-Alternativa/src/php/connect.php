e96a25d5c6b020e1a47accee6334eb1f1c2b295f
MartiniGlauber <gmartinisantos@gmail.com>
2024-07-29 21:07:39 -0300

    subindo site em testes

--- Proj-PI-Site-Alternativa/src/php/connect.php
+++ Proj-PI-Site-Alternativa/src/php/connect.php
@@ -2,7 +2,7 @@
 // connect.php
 $dsn = 'mysql:host=localhost;dbname=alternativa_autolocadora';
 $username = 'root';
-$password = 'root';
+$password = '';
 $options = array(
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
--- Proj-PI-Site-Alternativa/src/php/process-reserva.php
+++ Proj-PI-Site-Alternativa/src/php/process-reserva.php
@@ -26,5 +26,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } else {
         echo "Erro ao registrar a reserva.";
     }
+    
 }
 ?>
